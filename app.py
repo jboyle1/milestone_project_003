0eaab9437a0638a1c1a69d7cbfd0045dc7071826
Jed Boyle <jboyle1@pca.ac.uk>
2019-11-20 14:19:17 +0000

    Added catagory template pages.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/architecture')
+def architecture():
+    return render_template('architecture.html')
+
+
+@app.route('/commercial')
+def commercial():
+    return render_template('commercial.html')
+
+
+@app.route('/property')
+def property():
+    return render_template('property.html')
+
+
+@app.route('/construction')
+def construction():
+    return render_template('construction.html')
 
 
 if __name__ == "__main__":
